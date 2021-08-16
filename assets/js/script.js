1d0d8933954c469e9106ef5ee8e237fef62feac8
MaeveHughes <maevehughes@gmail.com>
2021-08-16 08:14:37 +0000

    Moving the styling from the javascript file to the css file

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+/* Google fonts */
+@import url('https://fonts.googleapis.com/css2?family=Asap+Condensed&display=swap');
+
+* {
+    margin: 0;
+    padding: 0;
+    border: none;
+}
+
+body {
+    font-family: Asap Condensed, sans-serif;
+    font-weight: 300px;
+    background-color: orange;
+    color: white;
+}
